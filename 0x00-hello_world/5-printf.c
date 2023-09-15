23ffac2f20b9e0dd25769c36b0644801d1438eb3
kitenga <vmvmuthiani@gmail.com>
2023-09-15 03:48:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program uses 'printf' to print a message
+ * followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
