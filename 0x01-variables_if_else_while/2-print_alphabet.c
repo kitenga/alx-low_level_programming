3b250851e7d85319a4ac0e852ef531353635bc33
kitenga <vmvmuthiani@gmail.com>
2023-09-16 15:21:21 -0700

    program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
